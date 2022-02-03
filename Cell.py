4000d28a5a4668b5118f8d77edb01bf9ac5a05b3
tleavitt25526 <98976857+tleavitt25526@users.noreply.github.com>
2022-02-03 17:53:00 -0500

    Update Cell.py

--- Cell.py
+++ Cell.py
@@ -1,3 +1,54 @@
-class Cell():
-    def __init__(self):
-        pass
+class Cell:
+    # boolean to see if cell has been passed by generator
+    passed = False
+    
+    # string to store direction cell traveled from. "O" is none
+    cameFrom = "O"
+    
+    # dictionary to store opposite directions
+    oppositeDirection = {
+        "N": "S",
+        "S": "N",
+        "E": "W",
+        "W": "E"
+    }
+    
+    # list to store all active walls (all start active)
+    wallList = ["N", "E", "S", "W"]
+    
+    # constructor
+    def __init__(self, x, y, cameFrom):
+        self.x = x;
+        self.y = y;
+    
+    # method to enter a cell and effect walls
+    def startPass(self, cameFrom):
+        self.cameFrom = cameFrom
+        passed = True
+        wallList.append(oppositeDirection[cameFrom])
+    
+    # method to exit a cell and effect walls
+    def endPass(self, goingTo):
+        wallList.append(goingTo)
+    
+    # set/get cameFrom variable
+    def setCameFrom(self, cameFrom):
+        self.cameFrom = cameFrom
+    def getCameFrom(self):
+        return self.cameFrom
+        
+    # set/get passed variable
+    def setPassed(self, passed):
+        self.passed = passed
+    def getPassed(self):
+        return self.passed
+    
+    # get and set methods for coordinates
+    def getY(self):
+        return self.y
+    def getX(self):
+        self.y=y
+    def getX(self):
+        return self.x
+    def setX(self, x):
+        self.x=x
