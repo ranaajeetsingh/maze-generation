691fce34eaee9476fb310fbff4572a4aae8a3afd
tleavitt25526 <98976857+tleavitt25526@users.noreply.github.com>
2022-02-05 14:40:43 -0500

    Update Cell.py

--- Cell.py
+++ Cell.py
@@ -1,41 +1,61 @@
-class Cell:
-    # boolean to see if cell has been passed by generator
-    passed = False
-    
-    # dictionary to store opposite directions
-    oppositeDirection = {
-        "N": "S",
-        "S": "N",
-        "E": "W",
-        "W": "E"
-    }
-    
-    # list to store all active walls (all start active)
-    wallList = ["N", "E", "S", "W"]
-    
-    # constructor
-    def __init__(self, x, y):
-        self.x = x;
-        self.y = y;
-    
-    # set/get cameFrom variable
-    def setCameFrom(self, cameFrom):
-        self.cameFrom = cameFrom
-    def getCameFrom(self):
-        return self.cameFrom
-        
-    # set/get passed variable
-    def setPassed(self, passed):
-        self.passed = passed
-    def getPassed(self):
-        return self.passed
-    
-    # get and set methods for coordinates
-    def getY(self):
-        return self.y
-    def getX(self):
-        self.y=y
-    def getX(self):
-        return self.x
-    def setX(self, x):
-        self.x=x
+import java.io.*;
+import java.util.*;
+
+public class Cell
+{
+   // hastable for edges
+   public Map<String, Boolean> edges = new HashMap<>();
+   
+   // hashtable for opposite edges
+   public Map<String, String> opposites = new HashMap<>();
+   
+   // boolean to check if the cell has been visited
+   public boolean visited;
+   
+   public int x, y;
+   
+   // direction the cell came from
+   public String direction = "O";
+   
+   // constructor
+   public Cell() {
+      // set up hash maps
+      setEdges();
+      setOpposites();
+   }
+   
+   public void discover() {
+      visited = true;
+   }
+   
+   // method to discover edge
+   public void discoverCell(Cell original) {
+      edges.replace(opposites.get(direction), false);
+      visited = true;
+      original.edges.replace(direction, false);
+   }
+   
+   // method to set edges
+   private void setEdges() {
+      edges.put("N", true);
+      edges.put("E", true);
+      edges.put("S", true);
+      edges.put("W", true);
+   }
+   
+   // method to set opposites
+   private void setOpposites() {
+      opposites.put("N", "S");
+      opposites.put("S", "N");
+      opposites.put("E", "W");
+      opposites.put("W", "E");
+   }
+}
+
+/* CELL STRUCTURE
+The structre of each cell represents a node with four edges, or walls.
+The cell contains it's own hashtable(String, boolean) for each edge.
+There are also methods to get the opposite edge given one.
+Other methods will come as I write out the code.
+The cell will also have an attribute to mark if they have been visited
+*/
