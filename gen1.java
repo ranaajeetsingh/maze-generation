072731f35f07139a440d7c1ca0260d42e09d4c73
tleavitt25526 <98976857+tleavitt25526@users.noreply.github.com>
2022-02-09 12:11:23 -0500

    Update gen1.java

--- gen1.java
+++ gen1.java
@@ -36,12 +36,51 @@ public class gen1 {
             }
         }
     }
+    /* SOUTH AND EAST VALUES
+    East is 4 and South is 1.
+    West is 8 and North is 2.
+    combinations are numbers 1-15. If a number is even, it has a bottom wall. 
+    east walls are 1, 2, 3, 4,8, 9, 10, 11
+    */
     
-    // method to display all values of grid
-    public void display(int[][] grid) {
+    // method to display grid as a text variable in the console
+    public void displayConsole(int[][] grid) {
+        // print top border
+        System.out.print(" ");
+        for (int i = 0; i < (grid[0].length * 2); i++) {
+            System.out.print("_");
+        }
+        System.out.println();
+        // loop through each row
+        for (int h = 0; h < grid.length; h++) {
+            System.out.print("|");
+            for (int w = 0; w < grid[h].length; w++) {
+                // testing for wall combinations for only south and east, since opposites are already assigned
+                // test if an east wall is present
+                if (grid[h][w] % 2 == 0) {
+                    System.out.print("_");
+                    if (grid[h][w] % 3 == 0) {
+                        System.out.print("|");
+                    }
+                    else System.out.print("_");
+                }
+                else System.out.print("  ");
+            }
+            //System.out.print("|");
+            System.out.println();
+        }
+        System.out.print("|");
+        for (int i = 0; i < (grid[0].length * 2); i++) {
+            System.out.print("_");
+        }
+        System.out.println("|");
+    }
+    
+    // method to display all values of grid in raw data
+    public void displayRaw(int[][] grid) {
         for (int x = 0; x < grid.length; x++) {
             for (int y = 0; y < grid[x].length; y++) {
-                System.out.print(grid[y][x] + " ");
+                System.out.print(grid[x][y] + " ");
             }
             System.out.println();
         }
